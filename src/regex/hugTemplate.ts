91d24739e701262e7edbb71dc4a0b808a7d6449b
Avinash Sinha <savinash2608@gmail.com>
2021-10-16 12:57:02 +0530

    add: cuddly hug

--- src/regex/hugTemplate.ts
+++ src/regex/hugTemplate.ts
@@ -1,1 +1,1 @@
-export const hugTemplateRegex = /\*(warm|tight|comfy|soft|snuggly)\s*hug\*\s*for\s*@[a-z0-9_]*/gim
+export const hugTemplateRegex = /\*(warm|tight|comfy|soft|snuggly|cuddly)\s*hug\*\s*for\s*@[a-z0-9_]*/gim
